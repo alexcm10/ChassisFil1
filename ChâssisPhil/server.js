c518b0fadfc96873247aed921133f39fa6cca3ad
alexcm10 <alexcid@live.be>
2025-08-28 17:20:26 +0200

    fix

---
+++ ChâssisPhil/server.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+    
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+    
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.jpeg':
+            contentType = 'image/jpeg';
+            break;
+        case '.gif':
+            contentType = 'image/gif';
+            break;
+    }
+    
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if(error.code === 'ENOENT') {
+                // Page non trouvée, rediriger vers index.html
+                fs.readFile('./index.html', (error, content) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf-8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end('Erreur serveur: ' + error.code);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+    console.log('Appuyez sur Ctrl+C pour arrêter le serveur');
+});
